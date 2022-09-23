187a5254c5f21a5b2506211347ac800207e1c9a5
AluaneX <96212771+karoSpacex@users.noreply.github.com>
2022-09-22 21:15:41 -0300

    Atualização do exercício 3
    Atualização com correção no cálculo.

--- ex3.cpp
+++ ex3.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 main (){
-	float A,B,C,delta;
+	float A,B,C,delta,x1,x2;
 	printf("Favor digite o valor correspondente a A: ");
 	scanf("%f",&A);
 	printf("Favor digite o valor correspondente a B: ");
@@ -9,5 +9,10 @@ main (){
 	printf("Favor digite o valor correspondente a C: ");
 	scanf("%f",&C);
 	delta = pow(B,2)-4*(A*C);
-	printf("DELTA = %f",delta);
+	printf("DELTA = %.2f\n",delta);
+	x1 = -B+sqrt(delta)/(2*A);
+	x2 = -B-sqrt(delta)/(2*A);
+	printf("Entao X1 eh: %f\n",x1);
+	printf("Entao x2 eh: %f",x2);
 }
+
